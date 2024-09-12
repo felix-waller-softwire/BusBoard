9cc6fc477c4ae629fa94c8b9a3bf4b25ba854fea
Felix Waller <felix.waller@softwire.com>
2024-09-12 13:49:21 +0100

    postcode

--- bustimes.rb
+++ bustimes.rb
@@ -2,27 +2,54 @@ require 'net/http'
 require 'json'
 
 class Bus_Board
-  def initialize(stop)
-    @stop = stop
-  end
-
   def next_5
-    arrivals = fetch_arrivals
-    arrivals
-      .sort {|a, b| a['timeToStation'] <=> b['timeToStation']}
-      .slice(0, 5)
-      .each do |arrival|
-        puts "#{arrival['lineName']} #{arrival['destinationName']} #{arrival['timeToStation'] / 60}"
-      end
+    @stops.each do |stop|
+      arrivals = fetch_arrivals(stop)
+      puts arrivals[0]['stationName']
+      arrivals
+        .sort {|a, b| a['timeToStation'] <=> b['timeToStation']}
+        .slice(0, 5)
+        .each do |arrival|
+          puts "#{arrival['lineName']} #{arrival['destinationName']} #{arrival['timeToStation'] / 60}"
+        end
+      puts
+    end
   end
 
-  private def fetch_arrivals
-    uri = URI("https://api.tfl.gov.uk/StopPoint/#{@stop}/Arrivals")
+  private def fetch_arrivals(stop)
+    uri = URI("https://api.tfl.gov.uk/StopPoint/#{stop}/Arrivals")
     res = Net::HTTP.get(uri)
     JSON.parse(res)
   end
 end
 
-# bus_board('490011445N')
-board = Bus_Board.new('490011445N')
+class Bus_Board_Stop < Bus_Board
+  def initialize(stop)
+    @stops = [stop]
+  end
+end
+
+class Bus_Board_Postcode < Bus_Board
+  def initialize(postcode)
+    uri = URI("https://api.postcodes.io/postcodes/#{postcode.gsub(' ', '%20')}")
+    res = Net::HTTP.get(uri)
+    data = JSON.parse(res)
+
+    lat = data['result']['latitude']
+    lon = data['result']['longitude']
+
+    uri = URI("https://api.tfl.gov.uk/StopPoint/?lat=#{lat}&lon=#{lon}&stopTypes=NaptanPublicBusCoachTram")
+    res = Net::HTTP.get(uri)
+    data = JSON.parse(res)
+
+    @stops = data['stopPoints'].map {|stop| stop['id']}
+  end
+end
+
+# 490008825N
+
+# board = Bus_Board_Stop.new(gets.chomp)
+# board.next_5
+
+board = Bus_Board_Postcode.new(gets.chomp)
 board.next_5
