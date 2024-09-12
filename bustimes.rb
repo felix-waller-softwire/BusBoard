b27790570df94757fbf700924cbbe7734df10696
Felix Waller <felix.waller@softwire.com>
2024-09-12 12:10:09 +0100

    oop

--- bustimes.rb
+++ bustimes.rb
@@ -1,20 +1,28 @@
 require 'net/http'
 require 'json'
 
-def bus_board(stop)
-  arrivals = fetch_arrivals(stop)
-  arrivals
-    .sort {|a, b| a['timeToStation'] <=> b['timeToStation']}
-    .slice(0, 5)
-    .each do |arrival|
-      puts "#{arrival['lineName']} #{arrival['destinationName']} #{arrival['timeToStation'] / 60}"
-    end
+class Bus_Board
+  def initialize(stop)
+    @stop = stop
+  end
+
+  def next_5
+    arrivals = fetch_arrivals
+    arrivals
+      .sort {|a, b| a['timeToStation'] <=> b['timeToStation']}
+      .slice(0, 5)
+      .each do |arrival|
+        puts "#{arrival['lineName']} #{arrival['destinationName']} #{arrival['timeToStation'] / 60}"
+      end
+  end
+
+  private def fetch_arrivals
+    uri = URI("https://api.tfl.gov.uk/StopPoint/#{@stop}/Arrivals")
+    res = Net::HTTP.get(uri)
+    JSON.parse(res)
+  end
 end
 
-def fetch_arrivals(stop)
-  uri = URI("https://api.tfl.gov.uk/StopPoint/#{stop}/Arrivals")
-  res = Net::HTTP.get(uri)
-  JSON.parse(res)
-end
-
-bus_board('490011445N')
+# bus_board('490011445N')
+board = Bus_Board.new('490011445N')
+board.next_5
