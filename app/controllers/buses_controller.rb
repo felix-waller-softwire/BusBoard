896eacc05939a54b52d791fb0ef3265acb57ed17
Felix Waller <felix.waller@softwire.com>
2024-09-13 11:05:42 +0100

    Refactor reusable functions

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -7,41 +7,47 @@ class BusesController < ApplicationController
 
   def stop
     stop = params[:stop]
-    uri = URI("https://api.tfl.gov.uk/StopPoint/#{stop}/Arrivals")
-    res = Net::HTTP.get(uri)
-    arrivals = JSON.parse(res)
-    arrivals
-      .sort_by! { |x| x['timeToStation'].to_i }
-      .slice(0, 5)
-    @buses = arrivals
+    @arrivals = fetch_arrivals([stop])
+    render :arrivals
   end
 
   def postcode
     postcode = params[:postcode]
+    lat, lon = fetch_postcode_coordinates(postcode)
+    stops = fetch_nearest_stops(lat, lon)
+    @arrivals = fetch_arrivals(stops)
+    render :arrivals
+  end
 
-    uri = URI("https://api.postcodes.io/postcodes/#{postcode.gsub(' ', '%20')}")
-    res = Net::HTTP.get(uri)
-    data = JSON.parse(res)
+  private
 
-    lat = data['result']['latitude']
-    lon = data['result']['longitude']
-
-    uri = URI("https://api.tfl.gov.uk/StopPoint/?lat=#{lat}&lon=#{lon}&stopTypes=NaptanPublicBusCoachTram")
-    res = Net::HTTP.get(uri)
-    data = JSON.parse(res)
-
-    stops = data['stopPoints'].map {|stop| stop['id']}
-
-    @stops = []
+  def fetch_arrivals(stops)
+    arrivals = []
 
     stops.each do |stop|
       uri = URI("https://api.tfl.gov.uk/StopPoint/#{stop}/Arrivals")
       res = Net::HTTP.get(uri)
-      arrivals = JSON.parse(res)
-      arrivals
+      buses = JSON.parse(res)
+      buses
         .sort_by! { |x| x['timeToStation'].to_i }
         .slice(0, 5)
-      @stops.push(arrivals)
+      arrivals.push(buses)
     end
+
+    arrivals
+  end
+
+  def fetch_nearest_stops(lat, lon)
+    uri = URI("https://api.tfl.gov.uk/StopPoint/?lat=#{lat}&lon=#{lon}&stopTypes=NaptanPublicBusCoachTram")
+    res = Net::HTTP.get(uri)
+    data = JSON.parse(res)
+    data['stopPoints'].map { |stop| stop['id'] }
+  end
+
+  def fetch_postcode_coordinates(postcode)
+    uri = URI("https://api.postcodes.io/postcodes/#{postcode.gsub(' ', '%20')}")
+    res = Net::HTTP.get(uri)
+    data = JSON.parse(res)
+    [data['result']['latitude'], data['result']['longitude']]
   end
 end
