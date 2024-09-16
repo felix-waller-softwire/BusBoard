558cf44b6fd734976709ea48ba8b131daead9011
Felix Waller <felix.waller@softwire.com>
2024-09-16 15:54:26 +0100

    Error handling

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -13,8 +13,13 @@ class BusesController < ApplicationController
 
   def postcode
     postcode = params[:postcode]
-    lat, lon = fetch_postcode_coordinates(postcode)
+
+    status, lat, lon = fetch_postcode_coordinates(postcode)
+    return render :index, locals: { err: :invalid_postcode } if status != 200
+
     stops = fetch_nearest_stops(lat, lon)
+    return render :index, locals: { err: :postcode_has_no_stops } if stops.nil? || stops.empty?
+
     @arrivals = fetch_arrivals(stops)
     render :arrivals
   end
@@ -22,32 +27,29 @@ class BusesController < ApplicationController
   private
 
   def fetch_arrivals(stops)
-    arrivals = []
-
-    stops.each do |stop|
+    stops.map do |stop|
       uri = URI("https://api.tfl.gov.uk/StopPoint/#{stop}/Arrivals")
       res = Net::HTTP.get(uri)
-      buses = JSON.parse(res)
-      buses
-        .sort_by! { |x| x['timeToStation'].to_i }
-        .slice(0, 5)
-      arrivals.push(buses)
-    end
+      data = JSON.parse(res)
 
-    arrivals
+      next if !data.kind_of?(Array)
+
+      data.sort_by { |x| x['timeToStation'].to_i }
+          .first(5)
+    end
   end
 
   def fetch_nearest_stops(lat, lon)
     uri = URI("https://api.tfl.gov.uk/StopPoint/?lat=#{lat}&lon=#{lon}&stopTypes=NaptanPublicBusCoachTram")
     res = Net::HTTP.get(uri)
     data = JSON.parse(res)
-    data['stopPoints'].map { |stop| stop['id'] }
+    data['stopPoints']&.map { |stop| stop['id'] }
   end
 
   def fetch_postcode_coordinates(postcode)
     uri = URI("https://api.postcodes.io/postcodes/#{postcode.gsub(' ', '%20')}")
     res = Net::HTTP.get(uri)
     data = JSON.parse(res)
-    [data['result']['latitude'], data['result']['longitude']]
+    [data['status'], data&.dig('result', 'latitude'), data&.dig('result', 'longitude')]
   end
 end
